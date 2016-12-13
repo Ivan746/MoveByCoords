fe4b06cdc04ab004698ed9007aee9a7d3d173d31
Ivan746 <ivanemelyanov@mail.ru>
2016-12-13 15:04:45 +0300

    Update Key.h

--- Key.h
+++ Key.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#pragma once
+
 char West[];
 char East[];
 char North[];
 char South[];
+char Str_Err1[];
+char Str_Err2[];
+char Str_Err3[];
+char Str_Err4[];
+char Str_Err5[];
+char Str_Err6[];
+char Str_Err7[];
+char Str_Err8[];
+char Str_Res[];
