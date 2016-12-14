bac8e74b27d4ba664f191143de0c57f690d3c240
Ivan746 <ivanemelyanov@mail.ru>
2016-12-14 16:16:52 +0300

    Update Key.h

--- Key.h
+++ Key.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#pragma once
-
 char West[];
 char East[];
 char North[];
@@ -15,3 +13,10 @@ char Str_Err6[];
 char Str_Err7[];
 char Str_Err8[];
 char Str_Res[];
+
+void My_Printer(int);
+void My_Printer(int, char*);
+void My_Printer(int, int);
+void My_Printer(int, char*, int);
+void My_Printer(int, int, int);
+
