3c1e0e22b42e329957d41cccc4fb90ead40a28a4
Ivan746 <ivanemelyanov@mail.ru>
2016-12-16 15:26:33 +0300

    Create ErrorPrintFunc.cpp

---
+++ ErrorPrintFunc.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Key.h"
+
+
+void Print_Enter()
+{
+	printf("\n");
+}
+
+void My_Printer(error numb, int count_str)
+{
+	switch(numb)
+	{
+		case(NotAWorldSide):
+			printf(StrErr_NotAWorldSide, count_str);
+			break;
+		case(OneWordOnly):
+			printf(StrErr2_OneWordOnly, count_str);
+			break;
+		case(EmptyStr):
+			printf(StrErr3_EmptyStr, count_str);
+			break;
+		case(CouldNotRead):
+			printf(StrErr4_CouldNotRead, count_str);
+			perror(" произошла ошибка");
+			break;
+	}
+	Print_Enter();
+}
+
+void My_Printer(error numb)
+{
+	switch(numb)
+	{
+		case(NotMemory):
+			printf(StrErr5_NotMemory);
+			break;
+		case(FileNotReceived):
+			printf(StrErr7_FileNotReceived);
+			break;
+		
+	}
+	Print_Enter();
+}
+
+void My_Printer(error numb, char* File_Name)
+{
+	switch(numb)
+	{
+		case (ErrorOpeningFile):
+			printf(StrErr6_ErrorOpeningFile, File_Name);
+			break;
+	}
+	Print_Enter();
+}
