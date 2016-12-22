1163186b56e61d610549f5a29ce373581d01d319
Ivan746 <ivanemelyanov@mail.ru>
2016-12-22 14:41:41 +0300

    Update MoveByCoords.cpp

--- MoveByCoords.cpp
+++ MoveByCoords.cpp
@@ -6,18 +6,13 @@
 #include <cstdio>
 #include <conio.h>
 #include <cstring>
-#include "stdarg.h"
 #include "Key.h"
 
 #define START_STR_SIZE 8
 #define INCREMENTS 4
 #define MAX_SIZE_STR_NUMB 20
-#define SIZE_TXT_FOR_PERROR 50
+#define NUMB_STR_ARRAY_MESS 4
 
-void Print_Enter()
-{
-	printf("\n");
-}
 
 void MagStr (char *str)
 {
@@ -75,16 +70,16 @@ void Bias(char *str, int count_str, int *x, int *y)
 						if (!strncmp(West, str, Numb_simv)){
 							*x -= atoi(buffer);
 						} else {
-							My_Printer(STR_NOT_A_WORLD_SIDE, count_str);
+							universal_print_messages(MES_NOT_A_WORLD_SIDE, count_str);
 						}
 					}
 				}
 			}
 		} else {
-			 My_Printer(STR_ONE_WORD_ONLY, count_str);
+			 universal_print_messages(MES_ONE_WORD_ONLY, count_str);
 		}
 	} else {
-		My_Printer(STR_EMPTY_STR, count_str);
+		universal_print_messages(MES_EMPTY_STR, count_str);
 	}
 }
 
@@ -94,63 +89,92 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	if (argc > 1){
 
-		FILE *file;
-		file = fopen(argv[1], "r");
+		int Numb_Str_Array_Mess = NUMB_STR_ARRAY_MESS;
+		char ** Array_Mess = NULL;
+		int was_error_Proc_Array_Mess = 0;
+
+		Array_Mess = Processing_Array_Message(&Numb_Str_Array_Mess, &was_error_Proc_Array_Mess, argv[1]);
+//		Data_collection_for_print(Array_Mess);
+
+		if(Array_Mess != NULL){
+
+			if (!was_error_Proc_Array_Mess){
 	
-		if(file != NULL){
+				if (argc > 2){
+	
+					FILE *file_with_coords;
+					file_with_coords = fopen(argv[2], "r");
+	
+					if(file_with_coords != NULL){
 			
-			int x = 0, y = 0, i = 0;
-			int was_error = 0;
+						int x = 0, y = 0, i = 0;
+						int was_error_in_main = 0;
 
-			char *buffer = (char*) malloc(sizeof(char) * START_STR_SIZE);
-			if (buffer != NULL){
-				
-				int count_str = 1;
-				int Str_Size = START_STR_SIZE;
+						char *buffer = (char*) malloc(sizeof(char) * START_STR_SIZE);
+						if (buffer != NULL){
+	
+							int count_str = 1;
+							int Str_Size = START_STR_SIZE;
 
-				while(!feof(file)){
+							while(!feof(file_with_coords)){
 					
-					if(i >= Str_Size){ 
-						Str_Size = (Str_Size + INCREMENTS);
-						buffer = (char*) realloc(buffer, ((Str_Size) * sizeof(char)));
+								if(i >= Str_Size){ 
+									Str_Size = (Str_Size + INCREMENTS);
+									buffer = (char*) realloc(buffer, ((Str_Size) * sizeof(char)));
 
-						if(buffer == NULL){
-							My_Printer(STR_NOT_MEMORY);
-							was_error = 1;
-							break;
-						}
-					}
+									if(buffer == NULL){
+										universal_print_messages(MES_NOT_MEMORY);
+										was_error_in_main = 1;
+										break;
+									}
+								}
 						
-					buffer[i] = getc(file);
+								buffer[i] = getc(file_with_coords);
 
-						if(!ferror(file)){ 
-							My_Printer(STR_COULD_NOT_READ, count_str, strerror(errno));
-							was_error = 1;
-							break;
+									if(ferror(file_with_coords)){ 
+										universal_print_messages(MES_COULD_NOT_READ, count_str, strerror(errno));
+										was_error_in_main = 1;
+										break;
+									}
+									if (buffer[i] == '\n'){
+										buffer[i] = '\0';
+										MagStr(buffer);
+										Bias(buffer, count_str, &x, &y);
+										count_str++;
+										i = -1; /** Для того чтобы запись каждой новой строки начиналась с 0 эллемента */
+									}
+									i++;
+							}
+							if(!was_error_in_main){
+								universal_print_messages(MES_RES_BIAS, x, y);
+							}
+							free(buffer);
+						} else {
+							universal_print_messages(MES_NOT_MEMORY);
 						}
-						if (buffer[i] == '\n'){
-							buffer[i] = '\0';
-							MagStr(buffer);
-							Bias(buffer, count_str, &x, &y);
-							count_str++;
-							i = -1; /** Для того чтобы запись каждой новой строки начиналась с 0 эллемента */
-						}
-						i++;
+						fclose(file_with_coords);
+					} else {
+						universal_print_messages(MES_ERROR_OPENING_FILE, argv[2]); 
+					}
+				} else {
+					universal_print_messages(MES_FILE_WITH_COORDS_NOT_RECEIVED);
 				}
-				if(!was_error){
-					printf(Str_Res, x, y);
-					Print_Enter();
-				}
-				free(buffer);
 			} else {
-				My_Printer(STR_NOT_MEMORY);
+				universal_print_messages(MES_ERROR_PROCESSING_FILE_WITH_MESSAGE, argv[2]);
 			}
-			fclose(file);
-		} else {
-			My_Printer(STR_ERROR_OPENING_FILE, argv[1]); 
+
+			for (int i = 0; i < Numb_Str_Array_Mess; i++){
+				free(Array_Mess[i]);
+			}
+			free(Array_Mess);
+
+		} else { 
+			universal_print_messages(MES_NOT_MEMORY);
 		}
 	} else {
-		My_Printer(STR_FILE_NOT_RECEIVED);
+	universal_print_messages(MES_FILE_WITH_MESSAGES_NOT_RECEIVED); 
 	}
+	
 	system("pause");
 }
+
