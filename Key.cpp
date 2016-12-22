f4370b355fdb70a38b462d91ab03d0af34580f70
Ivan746 <ivanemelyanov@mail.ru>
2016-12-22 14:21:07 +0300

    Update Key.cpp

--- Key.cpp
+++ Key.cpp
@@ -9,6 +9,7 @@ const char South[] = "Юг";
 const char StrErr_NotAWorldSide[] = "Cтрока № %d не совпадает ни с 1 из сторон света";
 const char StrErr_OneWordOnly[] = "Cтрока № %d состоит из 1 слова";
 const char StrErr_EmptyStr[] = "Cтрока № %d пустая";
+const char StrErr_CouldNotRead[] = "При считывании строки %d произошла ошибка : %s";
 const char StrErr_NotMemory[] = "Недостаточно памяти";
 const char StrErr_ErrorOpeningFile[] = "Ошибка открытия файла %s";
 const char StrErr_FileNotReceived[] = "Файл с координатами не получен\n Для работы с данным продуктом пожалуйста укажите путь к текстовому файлу содержащему координаты ";
