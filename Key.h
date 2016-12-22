ba84aa02383ec6a5c73a4f252410fd673d0d04c4
Ivan746 <ivanemelyanov@mail.ru>
2016-12-22 14:22:45 +0300

    Update Key.h

--- Key.h
+++ Key.h
@@ -13,3 +13,20 @@ extern const char StrErr_NotMemory[];
 extern const char StrErr_ErrorOpeningFile[];
 extern const char StrErr_FileNotReceived[];
 extern const char Str_Res[];
+
+enum STR_ERRORS{
+STR_NOT_A_WORLD_SIDE = 1,
+STR_ONE_WORD_ONLY,
+STR_EMPTY_STR,
+STR_COULD_NOT_READ,
+STR_NOT_MEMORY,
+STR_ERROR_OPENING_FILE,
+STR_FILE_NOT_RECEIVED
+};
+
+extern void My_Printer(STR_ERRORS WhenNotMem_or_FileNotReceived);
+extern void My_Printer(STR_ERRORS WhenErrorOpeningFile, char* NameFile);
+extern void My_Printer(STR_ERRORS WhenErrorInProcessingStr, int NumbStr);
+extern void My_Printer(STR_ERRORS CouldNotRead, int NumbStr, char* StrError);
+
+extern void Print_Enter();
