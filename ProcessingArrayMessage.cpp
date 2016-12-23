f27252450972cd391a24bdf4ad6aed00dc9e432a
Ivan746 <ivanemelyanov@mail.ru>
2016-12-24 01:02:37 +0300

    Update ProcessingArrayMessage.cpp

--- ProcessingArrayMessage.cpp
+++ ProcessingArrayMessage.cpp
@@ -2,61 +2,105 @@
 #include "Key.h"
 #include <iostream>
 
-char ** Processing_Array_Message(int *Numb_Str_Array_Mess, int *was_error_Proc_Array_Mess, char * File_Name)
+int Processing_Array_Message(int *Numb_Str_Array_Mess, char * File_Name)
 {
-		FILE *file_with_message;
-		file_with_message = fopen(File_Name, "r");
-		if(file_with_message != NULL){
+	FILE *file_with_message;
+	file_with_message = fopen(File_Name, "r");
+	if(file_with_message != NULL){
 
-			const int length_str = 256;
-			const int Increments = 2;
+		const int length_str = 256;
+		const int Increments = 2;
+		int was_error_Proc_Array_Mess = 0;
+		int numb_str_err_mem = 0;
 
-			char ** Array_Mess = (char **) malloc(sizeof(char *) * (*Numb_Str_Array_Mess));
+		Array_Mess = (char **) malloc(sizeof(char *) * (*Numb_Str_Array_Mess));
+
+		if(Array_Mess != NULL){
+
 			for(int i = 0; i < (*Numb_Str_Array_Mess); i++){
+
+				Array_Mess[i] = NULL;
 				Array_Mess[i] = (char*) malloc (sizeof(char) * length_str);
-			}
-			
-			if(Array_Mess != NULL){
 
-			int i = 0;
-
-			while(!feof(file_with_message)){
-
-				if(i >= *Numb_Str_Array_Mess){
-
-					*Numb_Str_Array_Mess = (*Numb_Str_Array_Mess + Increments);
-					Array_Mess = (char **) realloc(Array_Mess, *Numb_Str_Array_Mess * sizeof(char*));
-					for(int j = i; j < *Numb_Str_Array_Mess; j++){
-						Array_Mess[j] = (char*) malloc (sizeof(char) * length_str);
-					}
-
-					if(Array_Mess == NULL){
-							universal_print_messages(MES_NOT_MEMORY);
-							*was_error_Proc_Array_Mess = 1;
-							break;
-						}
-				}
-
-				if(fgets(Array_Mess[i], length_str, file_with_message) != NULL){
-				}
-				if(ferror(file_with_message)){ 
-					universal_print_messages(MES_COULD_NOT_READ, (i + 1), strerror(errno));
-					*was_error_Proc_Array_Mess = 1;
+				if(Array_Mess[i] == NULL){
+					numb_str_err_mem = i;
+					was_error_Proc_Array_Mess = 1;
 					break;
 				}
+			}
+			if(!was_error_Proc_Array_Mess){
+			
+				int i = 0;
+				int j = 0;
+
+				while(!feof(file_with_message)){
+
+					if(i >= *Numb_Str_Array_Mess){
+
+						*Numb_Str_Array_Mess = (*Numb_Str_Array_Mess + Increments);
+						Array_Mess = (char **) realloc(Array_Mess, *Numb_Str_Array_Mess * sizeof(char*));
+
+						if(Array_Mess != NULL){
+
+							for(j = i; j < *Numb_Str_Array_Mess; j++){
+
+								Array_Mess[j] = NULL;
+								Array_Mess[j] = (char*) malloc (sizeof(char) * length_str);
+
+								if(Array_Mess[j] == NULL){
+									numb_str_err_mem = j;
+									was_error_Proc_Array_Mess = 1;
+									break;
+								}
+							}
+						} else {
+							universal_print_messages(MES_NOT_MEMORY_SYS);
+							was_error_Proc_Array_Mess = 1;
+							break;
+						}
+					}
+
+					if(!(was_error_Proc_Array_Mess)){
 					
-				i++;
-			}
-			if(!(*was_error_Proc_Array_Mess)){
-				return Array_Mess;
-			} 
+						if(fgets(Array_Mess[i], length_str, file_with_message) != NULL){
+						}
+						if(ferror(file_with_message)){ 
+							universal_print_messages(MES_COULD_NOT_READ_SYS, (i + 1), strerror(errno));
+							was_error_Proc_Array_Mess = 1;
+							break;
+						}
+							i++;
+					} else {
+						universal_print_messages(MES_NOT_MEMORY_SYS);
+						break;
+					}
+				}
+				
+				if(!(was_error_Proc_Array_Mess)){
+					return 0;
+					
+				} else {
+					for (int i = 0; i < numb_str_err_mem; i++){
+						free(Array_Mess[i]);
+					}
+					free(Array_Mess);
+				}
 			} else {
-				universal_print_messages(MES_NOT_MEMORY);
+
+				for (int i = 0; i < numb_str_err_mem; i++){
+					free(Array_Mess[i]);
+				}
+				free(Array_Mess);
+				universal_print_messages(MES_NOT_MEMORY_SYS);
 			}
-			fclose(file_with_message);
 		} else {
-			universal_print_messages(MES_ERROR_OPENING_FILE, File_Name);
+			universal_print_messages(MES_NOT_MEMORY_SYS);
 		}
 
-		system("pause");
+		fclose(file_with_message);
+	} else {
+		universal_print_messages(MES_ERROR_OPENING_FILE_SYS, File_Name);
+	}
+	return 1;
 }
+
