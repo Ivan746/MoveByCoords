009d56a11f199e48ec6a940b38f1c1b1a487c42b
Ivan746 <ivanemelyanov@mail.ru>
2016-12-24 00:57:58 +0300

    Update ErrorPrintFunc.cpp

--- ErrorPrintFunc.cpp
+++ ErrorPrintFunc.cpp
@@ -2,57 +2,73 @@
 #include "stdarg.h"
 #include "Key.h"
 
+#define NUMB_SYS_MESSAGE 5
+#define DIFFER_BETW_DELIM_AND_FIRST_CHAR_MESS 2
+#define TRANSIT_MESS_FORMAT	10
+
 void Print_Enter()
 {
 	printf("\n");
 }
-/*
-void Data_collection_for_print(char **Array_Mess)
-{
-	Array_Mess;
-	
-}*/
+
 void universal_print_messages(MES_NUMBERS numb, ...)
 {
-	
-	va_list ptr;
+	int Numb_simv = 0;
 
+	if((numb > NUMB_SYS_MESSAGE) && (numb <= NUMB_SYS_MESSAGE + TRANSIT_MESS_FORMAT)){ /** если строк в файле 10 строк или менее */
+		char *adr_delimiter = NULL;
+		adr_delimiter = strchr(Array_Mess[0], '|'); 
+		Numb_simv = adr_delimiter - Array_Mess[0];
+		Numb_simv  +=  DIFFER_BETW_DELIM_AND_FIRST_CHAR_MESS; /** т.к. между разделителем и началом строки стоит пробел */                                          
+	}
+	
+	if((numb > NUMB_SYS_MESSAGE + TRANSIT_MESS_FORMAT)){ /** Если сообщений в файле оказывается более 10 (при наличии боле 101 строки требуется доработка)*/
+		char *adr_delimiter = NULL;
+		adr_delimiter = strchr(Array_Mess[10], '|');
+		Numb_simv = adr_delimiter - Array_Mess[10];
+		Numb_simv  +=  DIFFER_BETW_DELIM_AND_FIRST_CHAR_MESS; /** т.к. между разделителем и началом строки стоит пробел */                                          
+	}
+	va_list ptr;
+	
 	va_start(ptr, numb);
 
 	switch (numb)
 	{
+		case MES_COULD_NOT_READ_SYS:
+			vprintf(StrSysErr_CouldNotRead, ptr);
+			break;
+		case MES_NOT_MEMORY_SYS:
+			vprintf(StrSysErr_NotMemory, ptr);
+			break;
+		case MES_ERROR_OPENING_FILE_SYS:
+			vprintf(StrSysErr_ErrorOpeningFile, ptr);
+			break;
+		case MES_FILE_WITH_MESSAGES_NOT_RECEIVED_SYS:
+			vprintf(StrSysErr_FileWithMessagesNotReceived, ptr);
+			break;
+		case MES_ERROR_PROCESSING_FILE_WITH_MESSAGE_SYS:
+			vprintf(StrSysErr_ErrorProcessingFileWithMessage, ptr);
+			break;
 		case MES_NOT_A_WORLD_SIDE:
-			vprintf(StrErr_NotAWorldSide, ptr);
+			vprintf(Array_Mess[0] + Numb_simv, ptr);
 			break;
 		case MES_ONE_WORD_ONLY:
-			vprintf(StrErr_OneWordOnly, ptr);
+			vprintf(Array_Mess[1] + Numb_simv, ptr);
 			break;
 		case MES_EMPTY_STR:
-			vprintf(StrErr_EmptyStr, ptr);
-			break;
-		case MES_COULD_NOT_READ:
-			vprintf(StrErr_CouldNotRead, ptr);
-			break;
-		case MES_NOT_MEMORY:
-			vprintf(StrErr_NotMemory, ptr);
+			vprintf(Array_Mess[2] + Numb_simv, ptr);
 			break;
 		case MES_FILE_WITH_COORDS_NOT_RECEIVED:
-			vprintf(StrErr_FileWithCoordsNotReceived, ptr);
-			break;
-		case MES_FILE_WITH_MESSAGES_NOT_RECEIVED:
-			vprintf(StrErr_FileWithMessagesNotReceived, ptr);
-			break;
-		case MES_ERROR_OPENING_FILE:
-			vprintf(StrErr_ErrorOpeningFile, ptr);
-			break;
-		case MES_ERROR_PROCESSING_FILE_WITH_MESSAGE:
-			vprintf(StrErr_ErrorProcessingFileWithMessage, ptr);
+			vprintf(Array_Mess[3] + Numb_simv, ptr);
 			break;
 		case MES_RES_BIAS:
-			vprintf(Str_Res_Bias, ptr);
+			vprintf(Array_Mess[4] + Numb_simv, ptr);
 			break;
 			
 	}
-	Print_Enter();
+	if((numb < 6) || (numb == MES_RES_BIAS)){ // последняя строка в файле отображается иначе чем все остальные
+		Print_Enter();
+	}
 	va_end(ptr);
 }
+
