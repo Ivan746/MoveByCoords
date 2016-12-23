aedb576c77196e5e85142141a5e26002bf23613c
Ivan746 <ivanemelyanov@mail.ru>
2016-12-24 00:55:39 +0300

    Update Key.h

--- Key.h
+++ Key.h
@@ -5,34 +5,29 @@ extern const char East[];
 extern const char North[];
 extern const char South[];
 
-extern const char StrErr_NotAWorldSide[];
-extern const char StrErr_OneWordOnly[];
-extern const char StrErr_EmptyStr[];
-extern const char StrErr_CouldNotRead[];
-extern const char StrErr_NotMemory[];
-extern const char StrErr_ErrorOpeningFile[];
-extern const char StrErr_FileWithCoordsNotReceived[];
-extern const char StrErr_FileWithMessagesNotReceived[];
-extern const char StrErr_ErrorProcessingFileWithMessage[];
-extern const char Str_Res_Bias[];
+extern const char StrSysErr_CouldNotRead[];
+extern const char StrSysErr_NotMemory[];
+extern const char StrSysErr_ErrorOpeningFile[];
+extern const char StrSysErr_FileWithMessagesNotReceived[];
+extern const char StrSysErr_ErrorProcessingFileWithMessage[];
 
-//extern char **Array_Mess;
+extern char **Array_Mess;
 
-enum MES_NUMBERS{
-MES_NOT_A_WORLD_SIDE = 1,
+enum MES_NUMBERS{ /** Cначала нумеруются системные сообщения */
+MES_COULD_NOT_READ_SYS = 1,
+MES_NOT_MEMORY_SYS,
+MES_ERROR_OPENING_FILE_SYS,
+MES_FILE_WITH_MESSAGES_NOT_RECEIVED_SYS,
+MES_ERROR_PROCESSING_FILE_WITH_MESSAGE_SYS,
+MES_NOT_A_WORLD_SIDE,
 MES_ONE_WORD_ONLY,
 MES_EMPTY_STR,
-MES_COULD_NOT_READ,
-MES_NOT_MEMORY,
-MES_ERROR_OPENING_FILE,
 MES_FILE_WITH_COORDS_NOT_RECEIVED,
-MES_FILE_WITH_MESSAGES_NOT_RECEIVED,
-MES_ERROR_PROCESSING_FILE_WITH_MESSAGE,
 MES_RES_BIAS
 };
 
-//void Data_collection_for_print(char** Array_Mess);
-extern char ** Processing_Array_Message(int *Numb_Str_Array_Mess, int *was_error_Proc_Array_Mess, char * File_Name);
+extern int Processing_Array_Message(int *Numb_Str_Array_Mess, char * File_Name);
 extern void universal_print_messages(MES_NUMBERS numb, ...);
 
 extern void Print_Enter();
+
