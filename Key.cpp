0c5cc95b775b8c88961e412906d8c508deb8110b
Ivan746 <ivanemelyanov@mail.ru>
2016-12-24 00:57:21 +0300

    Update Key.cpp

--- Key.cpp
+++ Key.cpp
@@ -6,16 +6,10 @@ const char East[]  = "Восток";
 const char North[] = "Север";
 const char South[] = "Юг";
 
+/** List of system errors */
+const char StrSysErr_CouldNotRead[] = "При считывании строки %d произошла ошибка : %s";
+const char StrSysErr_NotMemory[] = "Недостаточно памяти"; 
+const char StrSysErr_ErrorOpeningFile[] = "Ошибка открытия файла %s"; 
+const char StrSysErr_FileWithMessagesNotReceived[] = "Файл с сообщениями программы не получен\n Для работы с данным продуктом пожалуйста укажите путь к текстовому файлу содержащему сообщения данной программы ";
+const char StrSysErr_ErrorProcessingFileWithMessage[] = "При обработке файла с сообщениями программы %s произошла ошибка"; 
 
-const char StrErr_CouldNotRead[] = "При считывании строки %d произошла ошибка : %s";//sys
-const char StrErr_NotMemory[] = "Недостаточно памяти"; // sys
-const char StrErr_ErrorOpeningFile[] = "Ошибка открытия файла %s"; // sys
-const char StrErr_FileWithMessagesNotReceived[] = "Файл с сообщениями программы не получен\n Для работы с данным продуктом пожалуйста укажите путь к текстовому файлу содержащему сообщения данной программы ";// sys
-const char StrErr_ErrorProcessingFileWithMessage[] = "При обработке файла с сообщениями программы %s произошла ошибка"; // sys
-
-
-const char StrErr_NotAWorldSide[] = "Cтрока № %d не совпадает ни с 1 из сторон света";
-const char StrErr_OneWordOnly[] = "Cтрока № %d состоит из 1 слова";
-const char StrErr_EmptyStr[] = "Cтрока № %d пустая";
-const char StrErr_FileWithCoordsNotReceived[] = "Файл с координатами не получен\n Для работы с данным продуктом пожалуйста укажите путь к текстовому файлу содержащему координаты ";
-const char Str_Res_Bias[] = "Финальная координата = %d, %d";
